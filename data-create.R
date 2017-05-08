df83a06d1280cfeaff3e7c07d20df325fabcb57e
jandremarais <maraisjandre9@gmail.com>
2017-05-08 15:28:54 +0200

    tiff and jpg summary and label mat code update

--- data-create.R
+++ data-create.R
@@ -1,112 +1,83 @@
 # data set creation
 
+path <- "."
+
 ## Label matrix
 
 library(tidyverse)
 library(stringr)
 
-train_labels <- read_csv("/home/jan/data/train.csv")
-rearrange_ind <- match(str_replace_all(list.files("/home/jan/data/train-jpg"), ".jpg", ""),
-                       train_labels$image_name)
-train_labels <- train_labels[rearrange_ind, ]
+train_labels <- read_csv(paste(path, "train.csv", sep = "/"))
 label_list <- strsplit(train_labels$tags, " ")
 L <- unique(unlist(label_list))
 
-Y <- sapply(L, function(b) sapply(label_list, function(a) ifelse(b %in% a, 1, 0)))
-write_csv(data.frame(Y), "/home/jan/data/labelmat.csv")
+Y <- ifelse(t(sapply(label_list, function(a) L %in% a)), 1, 0)
+colnames(Y) <- L
+#Y <- data.frame(id = train_labels$image_name, Y, stringsAsFactors = FALSE)
 
-## Basic features
+write_csv(data.frame(Y), "labelmat.csv")
+
+## Features
+
+### JPG
+
+#### Summary statistics
 
 library(raster)
 library(pbapply)
 library(moments)
 library(parallel)
 
-path <- '/home/jan/data/train-jpg'
+paste0(path, "/train-jpg/", train_labels$image_name, ".jpg")
 
-X_list <- mclapply(list.files(path), function(a) {
-  img <- brick(paste(path, a, sep = "/"))
-  c(apply(values(img), 2, function(a) {
-    c(mean = mean(a),
-      sd = sd(a),
-      min = min(a),
-      max = max(a),
-      kurt = kurtosis(a),
-      skew = skewness(a))
-  }))
-}, mc.cores = detectCores())
-
-X <- do.call("rbind", X_list)
-write_csv(data.frame(X), "/home/jan/data/X-sum-jpg.csv")
-
-path <- '/home/jan/data/test-jpg'
-
-X_list <- mclapply(list.files(path), function(a) {
-  img <- brick(paste(path, a, sep = "/"))
-  c(apply(values(img), 2, function(a) {
-    c(mean = mean(a),
-      sd = sd(a),
-      min = min(a),
-      max = max(a),
-      kurt = kurtosis(a),
-      skew = skewness(a))
-  }))
-}, mc.cores = detectCores())
-
-X_test <- do.call("rbind", X_list)
-write_csv(data.frame(X_test), "/home/jan/data/X-sum-jpg-test.csv")
-
-
-## Resize image to 32x32
-
-path <- '/home/jan/data/train-jpg'
-img <- brick(paste(path, "train_0.jpg", sep = "/"))
-svd_out <- svd(getValues(img$train_0.1, format = "matrix"))
-svd_out$u[, 1:64] %*% diag(svd_out$d[1:64]) %*% t(svd_out$v[, 1:64])
-
-ind <- seq(0, 256, 8)
-lapply(2:(length(ind)), function(a) {
-  width <- (ind[a-1]+1):(ind[a])
-  mean(values(img$train_0.1, format = "matrix")[width, width])
-})
-
-X_list <- mclapply(list.files(path)[1:10], function(a) {
-  img <- brick(paste(path, a, sep = "/"))
-  c(values(img))
-}, mc.cores = detectCores())
-
-X <- do.call("rbind", X_list)
-write_csv(data.frame(X), "/home/jan/data/X-sum-jpg.csv")
-
-
-## creaing a dataset from the histograms of the images using EBImage
-
-source("https://bioconductor.org/biocLite.R")
-biocLite("EBImage")
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("EBImage")
 
 library(EBImage)
-library(pbapply)
-library(parallel)
 
 nCores <- detectCores()
-path <- '/home/jan/data/train-jpg'
 
-X_list <- mclapply(list.files(path), function(a) {
-  img <- readImage(paste(path, a, sep = "/"))
-  hist_data <- hist(img)
-  unlist(lapply(hist_data, "[[", "density"))
+X_list <- mclapply(paste0(path, "/train-jpg/", train_labels$image_name, ".jpg"), function(a) {
+  img <- readImage(a)
+  img_data <- imageData(img)
+  c(sapply(1:3, function(b) {
+    dat <- c(img_data[,,b])
+    c(mean = mean(dat),
+      sd = sd(dat),
+      min = min(dat),
+      max = max(dat),
+      kurt = kurtosis(dat),
+      skew = skewness(dat))
+  }))
 }, mc.cores = nCores)
 
 X <- do.call("rbind", X_list)
-write_csv(data.frame(X), "/home/jan/data/X-hist-jpg.csv")
 
-path <- '/home/jan/data/test-jpg'
+colnames(X) <- c(sapply(c("R", "G", "B"), function(a) paste(a, c("mean", "sd", "min", "max", "kurtosis", "skewness"), sep = "_")))
+write_csv(data.frame(X), "X-summ-jpg.csv")
 
-X_list <- mclapply(list.files(path), function(a) {
-  img <- readImage(paste(path, a, sep = "/"))
-  hist_data <- hist(img)
-  unlist(lapply(hist_data, "[[", "density"))
-}, mc.cores = detectCores())
+### TIF
 
-X_test <- do.call("rbind", X_list)
-write_csv(data.frame(X_test), "/home/jan/data/X-hist-jpg-test.csv")
+#### Summary statistics
+
+X_list <- mclapply(paste0(path, "/train-tif-v2/", train_labels$image_name, ".tif"), function(a) {
+  img <- readImage(a)
+  img_data <- imageData(img)
+  c(sapply(1:4, function(b) {
+    dat <- c(img_data[,,b])
+    c(mean = mean(dat),
+      sd = sd(dat),
+      min = min(dat),
+      max = max(dat),
+      kurt = kurtosis(dat),
+      skew = skewness(dat))
+  }))
+}, mc.cores = nCores)
+
+X <- do.call("rbind", X_list)
+
+colnames(X) <- c(sapply(c("R", "G", "B", "NIR"), function(a) paste(a, c("mean", "sd", "min", "max", "kurtosis", "skewness"), sep = "_")))
+write_csv(data.frame(X), "X-summ-tif.csv")
+
+img_temp <- readImage("./train-tif-v2/train_1.tif")
+plot(img_temp)
